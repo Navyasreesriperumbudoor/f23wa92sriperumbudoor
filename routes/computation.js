9d96c2cc718854edc442fc738c6d8ef41842dff8
NAVYA SREE SRIPERUMBUDOOR <143027942+S567092@users.noreply.github.com>
2023-10-24 01:26:04 -0500

    added bonus files

---
+++ routes/computation.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Navyasree Sriperumbudoor' });
+if(req.query.x==undefined){
+  var RandomValue= Math.random();
+}
+else{
+  var tanhVal=Math.tanh(RandomValue);
+  res.render(`computation`,{tanh: `tanh(${tanh}) value is= ${tanhVal}`})
+}
+});
+
+module.exports = router;
