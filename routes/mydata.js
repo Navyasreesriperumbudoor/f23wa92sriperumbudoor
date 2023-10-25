b8dd2599182363da01c8879ed46a27844a4466bb
NAVYA SREE SRIPERUMBUDOOR <143027942+S567092@users.noreply.github.com>
2023-10-25 12:15:06 -0500

    added mydata.js file

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Navyasree Sriperumbudoor' });
+  res.render('mydata', { title: 'Navyasree Sriperumbudoor' });
 });
 
 module.exports = router;
